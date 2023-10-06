e7d42ed0125c2fef1ff29cc8235a686617b4f876
leeyuan <dbdks32123@gmail.com>
2023-10-06 15:19:58 +0900

    settimeout 출력 테스트

---
+++ app.js
@@ -0,0 +1,29 @@
+// setTimeout(function(){
+//   console.log('첫번째 소사');
+// }, 3000);
+
+// setTimeout(function(){
+//   console.log('두번째 뭉기');
+// }, 2000);
+
+// setTimeout(function(){
+//   console.log('세번째 밍구');
+// }, 1000);
+// settimeout은 스크립트 순서를 무시 : 밍구-> 뭉기-> 소사 순으로 출력 (순서대로 : 동기)
+
+setTimeout(function(){
+  console.log('첫번째 소사');
+
+  setTimeout(function(){
+  console.log('두번째 뭉기');
+
+      setTimeout(function(){
+      console.log('세번째 밍구');
+    }, 1000);
+
+  }, 2000);
+
+}, 3000);
+callback으로 settimeout 사용해서 순서 변경 : 소사 -> 뭉기 -> 밍구 (시간 단위로 : 비동기)
+
+
